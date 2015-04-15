dbb6570d80639e94db125006da05bd6767e825dc
Subrat Rout <subratnrout@gmail.com>
2015-04-14 20:38:29 -0700

    Added server.js, front end routes and mongoose

--- client/script.js
+++ client/script.js
@@ -1,1 +1,21 @@
-script.js
+  var ministore_app = angular.module('ministore_app', ['ngRoute']);
+
+  // Set up routing
+  ministore_app.config(function ($routeProvider) {
+      $routeProvider
+        .when('/',{
+            templateUrl: 'partials/customers.html'
+        })
+        .when('/customers',{
+            templateUrl: 'partials/customers.html'
+        })
+        .when('/orders',{
+            templateUrl: 'partials/orders.html'
+        })
+        .when('/products',{
+            templateUrl: 'partials/products.html'
+        })
+        .otherwise({
+          redirectTo: '/'
+        });
+    });
---
+++ server.js
@@ -0,0 +1,22 @@
+// require express
+var express = require('express');
+
+//require path
+var path = require('path');
+
+// instatntiate the app
+var app = express();
+
+// set up a static file server that points to "client" directory
+app.use(express.static(path.join(__dirname, './client')));
+
+// This goes in our server.js file so that we actually use the mongoose config file!
+// Load mongoose before route loading
+require('./config/mongoose.js');
+
+// require routes file
+//require('./config/routes.js')(app);
+
+app.listen(8000, function(){
+	console.log('Server listening to port 8000');
+})
